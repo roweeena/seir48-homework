2d8d1771cd94ce4f85101cf216c0febbe1201624
cheddachedda <daniel.n.q.nguyen@gmail.com>
2021-09-21 20:19:32 +1000

    half of Strings

---
+++ Daniel Nguyen/Week1/Day2/calculator/js/script.js
@@ -0,0 +1,62 @@
+// Week 1, Day 2
+// The Calculator
+
+// Part 1
+// with function statements
+
+function squareNumber(number) {
+  const squared = number * number;
+  console.log(`${number} squared is ${squared}.`);
+  return squared;
+}
+
+squareNumber(1212);
+
+function halfNumber(number) {
+  const halved = number / 2;
+  console.log(`Half of ${number} is ${halved}.`);
+  return halved;
+}
+
+halfNumber(71);
+
+// returns as a string with the '%' sign
+function percentOf(a, b) {
+  const percentage = a / b;
+  const percentageString = `${Math.round(percentage * 100)}%`;
+  console.log(`${a} is ${percentageString} of ${b}.`);
+  return percentageString;
+}
+
+percentOf(4, 17);
+
+// including the Bonus task
+function areaOfCircle(radius) {
+  const area = radius * radius * Math.PI;
+  // BONUS ROUND:
+  const roundedArea = area.toFixed(2);
+  console.log(`The area for a circle with radius ${radius} is ${roundedArea}.`);
+  return roundedArea;
+}
+
+areaOfCircle(8);
+
+
+
+// Part 2
+// with a function expression
+
+const magicNumber = function(number) {
+  console.log(`The magic number is ${number}`);
+  const halved = halfNumber(number);
+  const squared = squareNumber(halved);
+  const area = areaOfCircle(squared);
+  const result = percentOf(area, squared);
+  return result;
+}
+
+magicNumber(18);
+
+
+// Thanks for reading!
+// – Dan
---
+++ Daniel Nguyen/Week1/Day2/strings/js/script.js
@@ -0,0 +1,37 @@
+// Week 1, Day 2
+// Strings
+
+// DrEvil
+
+const drEvil = function(amount) {
+  let string = `DrEvil(${amount}): ${amount} dollars`;
+  // '==' rather than '===' to allow for string arguments
+  if (amount == 1000000) {
+    string += ' (pinky)';
+  }
+  console.log(string);
+  return string;
+}
+
+// TESTS
+console.log("Dr Evil tests:");
+drEvil(999999);
+drEvil(1000000);
+drEvil("1000000");
+drEvil(1000001);
+
+
+// MixUp
+
+const mixUp = function(stringOne, stringTwo) {
+  const firstWord = stringTwo.slice(0,2) + stringOne.slice(2);
+  const secondWord = stringOne.slice(0,2) + stringTwo.slice(2);
+  const originalWords = stringOne + ' ' + stringTwo;
+  const spoonerism = firstWord + ' ' + secondWord;
+  console.log(`${originalWords} becomes ${spoonerism}`);
+  return spoonerism;
+}
+
+mixUp("mix", "pod");
+mixUp("dog", "dinner");
+mixUp("Java", "Script");
