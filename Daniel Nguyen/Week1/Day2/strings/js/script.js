82668d4fc1a0c78ec3e28691072cc73d389d5a29
cheddachedda <daniel.n.q.nguyen@gmail.com>
2021-09-21 23:15:16 +1000

    Calculator complete, Strings 4/5

--- Daniel Nguyen/Week1/Day2/strings/js/script.js
+++ Daniel Nguyen/Week1/Day2/strings/js/script.js
@@ -13,7 +13,6 @@ const drEvil = function(amount) {
   return string;
 }
 
-console.log("Dr Evil tests:");
 drEvil(999999);
 drEvil(1000000);
 drEvil("1000000");
@@ -39,5 +38,48 @@ mixUp("Java", "Script");
 // FixStart
 
 function fixStart(string) {
-  
+  let output = string[0];
+  for (let i = 1; i < string.length; i++) {
+    if (string[i] === string[0]) {
+      output += '*';
+    } else {
+      output += string[i];
+    }
+  }
+  console.log(output);
+  return output;
 }
+
+fixStart("babble");
+fixStart("fluffer");
+
+
+// Verbing
+
+function verbing(string) {
+  let output = string;
+  const lastThreeChars = string.substring(string.length - 3);
+  // A "gerund" is the term for verb ending with "ing"
+  const gerund = lastThreeChars === "ing";
+
+  if (string.length > 3)
+    output += gerund ? "ly" : "ing";
+
+  console.log(output);
+  return output;
+}
+
+verbing('swim');
+verbing('swimming');
+verbing('go');
+
+
+// Not Bad
+
+function notBad(string) {
+
+}
+
+notBad('This dinner is not that bad!');
+notBad('This movie is not so bad!');
+notBad('This dinner is bad!');
