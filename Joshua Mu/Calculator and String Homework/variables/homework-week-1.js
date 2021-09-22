a711eecd493140883551042790452a91af6c4361
Joshua <shadicmasters@gmail.com>
2021-09-22 10:59:27 +1000

    Completed Homework
    Everything had been completed and everything moved into folder for ease of access

--- Joshua Mu/Calculator and String Homework/variables/homework-week-1.js
+++ Joshua Mu/Calculator and String Homework/variables/homework-week-1.js
@@ -1,7 +1,6 @@
 //Calculator
 
-function squareNumber(_int)
-{
+function squareNumber(_int) {
     _squaredNumber = _int * _int;
     console.log(`This result of squaring the number ${_int} is ${_squaredNumber}`);
     return _squaredNumber;
@@ -9,8 +8,7 @@ function squareNumber(_int)
 
 squareNumber(3);
 
-function halfNumber(_oneOverTwo)
-{
+function halfNumber(_oneOverTwo) {
     _halfNumber = _oneOverTwo / 2;
     console.log(`Half of ${_oneOverTwo} is ${_halfNumber}`);
     return _halfNumber;
@@ -18,8 +16,7 @@ function halfNumber(_oneOverTwo)
 
 halfNumber(4);
 
-function percentOf(_firstInteger, _secondInteger)
-{
+function percentOf(_firstInteger, _secondInteger) {
     _percentage = (_firstInteger / _secondInteger) * 100;
     console.log(`${_firstInteger} is ${_percentage}% of ${_secondInteger}`);
     return _percentage;
@@ -27,10 +24,9 @@ function percentOf(_firstInteger, _secondInteger)
 
 percentOf(2, 4);
 
-function areaOfCircle(_radius)
-{
+function areaOfCircle(_radius) {
     _pi = Math.PI;
-    _circleArea = _pi * _radius^2;
+    _circleArea = _pi * _radius ^ 2;
     _limitedNumber = parseFloat(_circleArea).toFixed(2);
     console.log(`The area for a circle with radius ${_radius} is ${_limitedNumber}`);
     return _limitedNumber;
@@ -38,14 +34,13 @@ function areaOfCircle(_radius)
 
 areaOfCircle(4);
 
-function calculatorThing(_calculatorNumber)
-{
+function calculatorThing(_calculatorNumber) {
     _halvedNumber = halfNumber(_calculatorNumber);
     _squared = squareNumber(_halvedNumber);
     _circledArea = areaOfCircle(_squared);
     _percentaged = percentOf(_circleArea, _squared);
     console.log(`The ${_calculatorNumber} when halved is ${_halvedNumber}, which when squared is ${_squared} `
-    + `Which then used to calculate the area of a triangle is ${_circledArea}. Makes a percentage of ${_percentaged}`);
+        + `Which then used to calculate the area of a triangle is ${_circledArea}. Makes a percentage of ${_percentaged}`);
     return _percentaged;
 }
 
@@ -53,26 +48,21 @@ calculatorThing(5);
 
 //String
 
-function DrEvil(_money)
-{
-    if (_money === 1000000)
-    {
+function DrEvil(_money) {
+    if (_money === 1000000) {
         console.log(`${_money} dollars (pinky)`);
     }
-    if (_money === 1)
-    {
+    if (_money === 1) {
         console.log(`${_money} dollar`);
     }
-    if (_money !== 1 && _money !== 1000000)
-    {
+    if (_money !== 1 && _money !== 1000000) {
         console.log(`${_money} dollars`);
     }
 }
 
 DrEvil(1000000);
 
-function MixUp(_firstWord, _secondWord)
-{
+function MixUp(_firstWord, _secondWord) {
     _lettersOne = _firstWord.substring(0, 2);
     _letters2 = _secondWord.substring(0, 2);
 
@@ -85,8 +75,7 @@ function MixUp(_firstWord, _secondWord)
 
 MixUp("Hello", "World");
 
-function FixStart(_string)
-{
+function FixStart(_string) {
     _initialLetter = _string.substring(0, 1);
     _replacedString = _string.substring(1, _string.length);
     _re = new RegExp(_initialLetter, 'g');
@@ -96,3 +85,41 @@ function FixStart(_string)
 }
 
 FixStart("aaaaaaaaaaaah");
+
+function Verbing(_verb) {
+    _newVerb = "";
+    if (_verb.length - 1 >= 3) {
+        if (_verb.slice(-3) === "ing") {
+            _newVerb = _verb + "ly";
+        }
+        else if (_verb.slice(-3) !== "ing") {
+            _newVerb = _verb + "ing";
+        }
+    }
+    else if (_verb.length - 1 < 3)
+    {
+        _newVerb = _verb;
+    }
+
+    console.log(_newVerb);
+}
+
+Verbing("swim");
+Verbing("swimming")
+Verbing("go");
+
+function NotBad(_notBad)
+{
+    locationOfNot = _notBad.indexOf("not");
+    locationOfBad = _notBad.indexOf("bad");
+
+    if (locationOfBad > -1 && locationOfNot > -1)
+    {
+        _good = _notBad.replace(_notBad.substring(locationOfNot, locationOfBad + 3), "");
+        _good = _good + "good";
+    }
+
+    console.log(_good);
+}
+
+NotBad("This is not bad");
