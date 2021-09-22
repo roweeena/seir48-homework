362e740409475ddf41d8051d4d180d1eb5e8e6e0
Your Name <you@example.com>
2021-09-22 11:25:34 +1000

    Commiting day 3 exercisws

---
+++ Suraj Sharma/week1/exercises.js
@@ -0,0 +1,48 @@
+const  numberofChildren = 4
+const partnersName = "steve"
+const partnersLocation = "India"
+const jobTitle = "Marketing"
+console.log('Hello')
+
+// ## Part 1
+//  //  Write a function called squareNumber that will take one argument (a number), square that number, and return the result. It should also log a string like "The result of squaring the number 3 is 9."
+
+const squareNumber = function(num){
+
+const squareresult = (num * num)
+  console.log(squareresult + "The  result of sqaring the number")
+
+}
+
+squareNumber(6)
+squareNumber(3)
+// - Write a function called halfNumber that will take one argument (a number), divide it by 2, and return the result. It should also log a string like "Half of 5 is 2.5.".
+const halfnumber = function(num){
+
+const halfnumber = (5 / 2)
+console.log(halfnumber + "Half of five is 2.5,")
+
+}
+halfnumber(2.5)
+
+// - Write a function called percentOf that will take two numbers, figure out what percent the first number represents of the second number, and return the result. It should also log a string like "2 is 50% of 4."
+const percentOf = function(num , num){
+const percentOf(num1 / num2)
+console.log(2 is 50% Of 4)
+return the result
+}
+// - Write a function called areaOfCircle that will take one argument (the radius), calculate the area based on that, and return the result. It should also log a string like "The area for a circle with radius 2 is 12.566370614359172."
+
+const radius = 1;
+const area = 2 * radius * math . pow 12
+console.log(the area for a circle is 2 of 12.566370614359172)
+}
+return the result
+// - Bonus: Round the result so there are only two digits after the decimal.
+//
+// ## Part 2
+// Write a function that will take one argument (a number) and perform the following operations, using the functions you wrote earlier1:
+// - Take half of the number and store the result.
+// - Square the result of #1 and store that result.
+// - Calculate the area of a circle with the result of #2 as the radius.
+// - Calculate what percentage that area is of the squared result (#3).
