3eee73496d1314a9e94ad64d04f8cac6c22d54b3
Joshua <shadicmasters@gmail.com>
2021-09-23 12:30:01 +1000

    Object Exercises

---
+++ Joshua Mu/Class Work/23-09-21/OBJECTS Exercises/objectScript.js
@@ -0,0 +1,70 @@
+const recipe = {
+    title: "Mole",
+    serves: 2,
+    ingredients: ["cinnamon", "cumin", "cocoa"]
+};
+
+console.log(recipe.title);
+console.log(recipe.serves);
+console.log(recipe.ingredients);
+
+const book1 = {
+    title: "Rangers Apprentice",
+    author: "John Flanagan",
+    read: true
+};
+
+const book2 = {
+    title: "Percy Jackson",
+    author: "Rick Riordan",
+    read: false
+};
+
+const library = [book1, book2];
+
+function CheckIfRead(books)
+{
+    for (let i = 0; i < books.length; i++)
+    {
+        if (books[i].read)
+        {
+            console.log(`The book ${books[i].title} by ${books[i].author} has been read`);
+        }
+        else{
+            console.log(`The book ${books[i].title} by ${books[i].author} has not been read`);
+        }
+    }
+}
+
+CheckIfRead(library);
+
+const movie1 = {
+    title: "Avengers Endgame",
+    author: "Marvel",
+    hours: 3,
+    minutes: 45,
+    stars: ["RDJ", " Chris Evans"]
+};
+
+const movie2 = {
+    title: "Avengers Infinity War",
+    author: "Marvel",
+    hours: 2,
+    minutes: 12,
+    stars: ["RDJ", " Chris Evans"]
+};
+
+const theatre = [movie1, movie2];
+
+function MovieInfo(movies)
+{
+    for (let i = 0; i < movies.length; i++)
+    {
+        console.log(`${movies[i].title} lasts for ${movies[i].hours} hours and ${movies[i].minutes} minutes. It stars ${movies[i].stars}`);
+    }
+}
+
+MovieInfo(theatre);
+
+
+
