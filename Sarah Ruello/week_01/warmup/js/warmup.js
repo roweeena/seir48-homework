c7dd025101515f239ef3ff08a45daa1facd765aa
Sarah <sarahjune@gmail.com>
2021-09-23 10:26:12 +1000

    Tidying up

--- Sarah Ruello/week_01/warmup/js/warmup.js
+++ Sarah Ruello/week_01/warmup/js/warmup.js
@@ -17,3 +17,28 @@ function raindrop(input) {
 console.log(raindrop(28));
 console.log(raindrop(34));
 console.log(raindrop(1755));
+
+// Warmup - Simon Says
+// Create a function that takes an input and returns Simon's response as follows:
+//  Simon answers 'Sure.' if you ask him a question.
+//  He answers 'Woah, chill out!' if you yell at him (ALL CAPS).
+//  He says 'Fine. Be that way!' if you address him without actually saying anything.
+//  He answers 'Whatever.' to anything else
+
+function simonSays(input) {
+  if (input.includes("?") && input !== input.toUpperCase()) {
+    return "Sure";
+  } else if (input === "") {
+    return "Fine. Be that way!;";
+  } else if (input == input.toUpperCase()) {
+    return "Woah, chill out!";
+  } else {
+    return "Whatever";
+  }
+}
+
+console.log(simonSays("Hey are you there?")); // Sure
+console.log(simonSays("LISTEN TO ME")); // Whoa chill out!
+console.log(simonSays("")); // Fine. Be that way!
+console.log(simonSays("Do your laundry.")); // Whatever
+console.log(simonSays("I'M TALKING TO YOU?")); // Whoa chill out!
