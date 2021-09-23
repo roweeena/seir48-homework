774313b889f7203afd74dfd582f5499f21707a93
daniel-nguyen <daniel.n.q.nguyen@gmail.com>
2021-09-23 20:03:00 +1000

    Geometry Function Lab

---
+++ Daniel Nguyen/Week1/Day4/geometry/js/script.js
@@ -0,0 +1,82 @@
+// # Geometry Function Lab
+
+// ### Part 1, Rectangle
+
+// Given the following a `rectangle` object like the one below, write the following functions which accept a `rectangle` object as an argument:
+
+const rectangleA = {
+  length: 4,
+  width: 4
+};
+
+// * isSquare - Returns whether the rectangle is a square or not
+const isSquare = function(rectangle) {
+  return rectangle.length === rectangle.width;
+}
+
+console.log('isSquare', isSquare(rectangleA));
+
+// * area - Returns the area of the rectangle
+const areaOfRectangle = function(rectangle) {
+  return rectangle.length * rectangle.width;
+}
+
+console.log('areaOfRectangle', areaOfRectangle(rectangleA));
+
+// * perimeter - Returns the perimeter of the rectangle
+const perimeter = function(rectangle) {
+  return (rectangle.length + rectangle.width) * 2;
+}
+
+console.log('perimeter', perimeter(rectangleA));
+
+
+// ### Part 2, Triangle
+
+// Given the following a `triangle` object like the one below, write the following functions which accept a `triangle` object as an argument:
+
+const triangleA = {
+  sideA: 3,
+  sideB: 4,
+  sideC: 4
+};
+
+// * isEquilateral - Returns whether the triangle is equilateral or not
+const isEquilateral = function(triangle) {
+  return triangle.sideA === triangle.sideB && triangle.sideA === triangle.sideC
+}
+
+console.log('isEquilateral', isEquilateral(triangleA));
+
+// * isIsosceles - Returns whether the triangle is isosceles or not
+const isIsosceles = function(triangle) {
+  const lengths = Object.values(triangle);
+  let unique = lengths.filter((side, i, arr) => arr.indexOf(side) === i);
+  return unique.length === 2;
+}
+
+console.log('isIsosceles', isIsosceles(triangleA));
+
+// * area - Returns the area of the Triangle
+const areaOfTriangle = function(triangle) {
+  const base = triangle.sideA;
+  const height = Math.sqrt(triangle.sideC - triangle.sideA/2);
+  return base * height / 2;
+}
+
+console.log('areaOfTriangle', areaOfTriangle(triangleA));
+
+// * isObtuse - Returns whether the triangle is obtuse or not
+const isObtuse = function(triangle) {
+  const sides = Object.values(triangle);
+  const sortedSides = Object.values(triangle).sort((a, b) => a - b);
+
+  // Where a < b < c...
+  const a = sortedSides[0];
+  const b = sortedSides[1];
+  const c = sortedSides[2];
+
+  return c*c > (a*a + b*b)
+}
+
+console.log('isObtuse', isObtuse(triangleA));
