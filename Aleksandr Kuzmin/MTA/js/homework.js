b6b1b865d165e8256f709a5957da559339d575aa
Aleksandr Kuzmin <kuzminbass@gmail.com>
2021-09-26 14:34:32 +1000

    adding more lines

--- Aleksandr Kuzmin/MTA/js/homework.js
+++ Aleksandr Kuzmin/MTA/js/homework.js
@@ -16,42 +16,86 @@ const lineL = ["8th", "6th", "Union Square", "3rd","1st"];
 const lineN = ["Times Square", "34th", "28th", "23rd", "Union Square","8th"];
 const line6 = ["Grand Central", "33rd", "28th", "23rd", "Union Square", "Astor Place"];
 
+const linesObj = {
+  line6: ["8th", "6th", "Union Square", "3rd","1st"],
+  lineN: ["Times Square", "34th", "28th", "23rd", "Union Square","8th"],
+  lineL: ["Grand Central", "33rd", "28th", "23rd", "Union Square", "Astor Place"]
 
+};
+
+//////////////////////////////////////////Choosing lines/////////////////////
+const linesArr =["N" , "6" , "L"]
+
+const choiceFromLine = prompt("Please choose the line where you start your journey?");
+const indexSearchLinefrom = linesArr.indexOf(choiceFromLine); // choosing a start line
+
+const choiceToLine = prompt("Please choose the line of your final stop");
+const indexSearchLineTo = linesArr.indexOf(choiceToLine); // choosing a destination line
+
+
+//////////////////////////////////////
 const choiceFrom = prompt("Where would you like to start you journey?");
-const indexSearchFromL = lineL.indexOf(choiceFrom);
 
-const choiceTo = prompt("Where would you like to go?");
-const indexSearchToL = lineL.indexOf(choiceTo);
+const choiceTo = prompt("Which station would you like to go?");
+const indexSearchFromL = lineL.indexOf(choiceFrom);
+const indexSearchFrom6 = line6.indexOf(choiceFrom);
+const indexSearchFromN = lineN.indexOf(choiceFrom);
+
+let indexSearchToL = lineL.indexOf(choiceTo);
+let indexSearchTo6 = line6.indexOf(choiceTo);
+let indexSearchToN = lineN.indexOf(choiceTo);
+
+
+
+
 
 function planTrip (choiceFrom, choiceTo) {
+///////////////////////////////////////////////////////////
+// If choice not found on L line go to Union Square
+  function needUs(indexSearchToL) {
+  if(indexSearchToL < 0) {
+    indexSearchToL = lineL.indexOf("Union Square");
+  }
+  return indexSearchToL;
+  }
+  indexSearchToL = needUs(indexSearchToL);
 
-  let numberOfStops  = indexSearchToL - indexSearchFromL;
-  console.log(Math.abs(numberOfStops) + " stops in total.");
+
+  let numberOfStopsL  = indexSearchToL - indexSearchFromL;
+  let printedNumStopsL = Math.abs(numberOfStopsL) - 1;
+  //let printedNumStops6 = Math.abs(numberOfStops6) - 1;
+  if(printedNumStopsL=== 1) {
+  console.log(printedNumStopsL + " stop in total.");
+} if (printedNumStopsL >= 1) {
+  console.log(printedNumStopsL + " stops in total.");
+
+}
+////////////////////////////////////////////////////////////// One line
 
 
 
-   if(numberOfStops < 0) {
+   if(numberOfStopsL < 0) {
   const reverseL = lineL.reverse()
   const indexSearchToL = reverseL.indexOf(choiceTo);
   const indexSearchFromL = reverseL.indexOf(choiceFrom);
-  const printedStations = lineL.slice(indexSearchFromL,indexSearchToL); // slicing the needed stations
-  printedStations.shift() // removes the start station
-  const separatedStations = printedStations.join(); // turn array into separate strings
-  console.log("Your journey continues through the following stops:  " + separatedStations + ".");
-  return separatedStations;
+  const printedStationsL = lineL.slice(indexSearchFromL,indexSearchToL); // slicing the needed stations
+  printedStationsL.shift() // removes the start station
+  const separatedStationsL = printedStationsL.join(); // turn array into separate strings
+  console.log("You must travel through the following stops on the L line:  " + separatedStationsL + ".");
+  return separatedStationsL;
 
 }
-  if(numberOfStops > 0) {
+  if(numberOfStopsL > 0) {
 
-   const printedStations = lineL.slice(indexSearchFromL,indexSearchToL); // slicing the needed stations
-   printedStations.shift() // removes the start station
-   const separatedStations = printedStations.join(); // turn array into separate strings
+   const printedStationsL = lineL.slice(indexSearchFromL,indexSearchToL); // slicing the needed stations
+   printedStationsL.shift() // removes the start station
+   const separatedStationsL = printedStationsL.join(); // turn array into separate strings
 
-   console.log("Your journey continues through the following stops: " + separatedStations + ".");
-   return separatedStations;
+   console.log("You must travel through the following stops on the L line: " + separatedStationsL + ".");
+   return separatedStationsL;
 }
 
-
+////////////////////////////////////////////////////////////////
 
 
 //for(let i = indexSearchFrom ; i < indexSearchTo; i++) {
