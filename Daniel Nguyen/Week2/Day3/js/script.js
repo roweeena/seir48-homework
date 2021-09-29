bc9bab7b27fd3e9342ebf26a58f8c52390dc83c0
daniel-nguyen <daniel.n.q.nguyen@gmail.com>
2021-09-29 17:41:51 +1000

    Day 8 cat-walk sans bonuses

---
+++ Daniel Nguyen/Week2/Day3/js/script.js
@@ -0,0 +1,14 @@
+const image = document.getElementById('cat');
+image.style.left = '0px';
+
+const parsePixels = function (px) {
+  return Number(px.split('px')[0]);
+}
+
+const catWalk = function () {
+  const current = parsePixels(image.style.left);
+  const next = current + 10;
+  image.style.left = next + 'px';
+}
+
+setInterval(catWalk, 50);
