8808fc9bac8df1e636063f707527e68f1eee0951
daniel-nguyen <daniel.n.q.nguyen@gmail.com>
2021-09-29 17:45:38 +1000

    Add function comment

--- Daniel Nguyen/Week2/Day3/js/script.js
+++ Daniel Nguyen/Week2/Day3/js/script.js
@@ -1,6 +1,7 @@
 const image = document.getElementById('cat');
 image.style.left = '0px';
 
+// Converts a CSS pixels string into a JS Number
 const parsePixels = function (px) {
   return Number(px.split('px')[0]);
 }
