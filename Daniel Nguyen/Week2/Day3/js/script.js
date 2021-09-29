156d4ca3d1ac89f7a6338efa4c14b9a8641ce8e9
daniel-nguyen <daniel.n.q.nguyen@gmail.com>
2021-09-29 23:29:18 +1000

    cat-walk bonus #2

--- Daniel Nguyen/Week2/Day3/js/script.js
+++ Daniel Nguyen/Week2/Day3/js/script.js
@@ -1,13 +1,11 @@
-const cat = document.getElementById('cat');
-
-const boundary = window.innerWidth - cat.width;
-
 // Converts a CSS pixels string into a JS Number
 const parsePixels = function (px) {
   return Number(px.split('px')[0]);
 }
 
-const catWalk = function () {
+const loop = function () {
+  const cat = document.getElementById('loop-cat');
+  const boundary = window.innerWidth - cat.width;
   const current = parsePixels(cat.style.left);
   const next = current + 1.5;
   cat.style.left = next + 'px';
@@ -16,4 +14,19 @@ const catWalk = function () {
   }
 }
 
-const timer = setInterval(catWalk, 1);
+let forwards = true;
+const backAndForth = function () {
+  const cat = document.getElementById('back-and-forth-cat');
+  const boundary = window.innerWidth - cat.width;
+  const current = parsePixels(cat.style.left);
+  const next = forwards ? current + 1.4 : current - 1.4;
+  cat.style.left = next + 'px';
+  if (forwards && next >= boundary) {
+    forwards = false;
+  } else if (!forwards && next <= 0) {
+    forwards = true;
+  }
+}
+
+setInterval(loop, 1);
+setInterval(backAndForth, 1.1);
