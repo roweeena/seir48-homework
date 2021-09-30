dc80716476f46e4d88a37fa622ce7af319cbf59f
daniel-nguyen <daniel.n.q.nguyen@gmail.com>
2021-10-01 09:37:28 +1000

    Day 10 warmup

---
+++ Daniel Nguyen/warmups/Week2/Day5/js/script.js
@@ -0,0 +1,42 @@
+// # Mumble
+
+// Write a function called mumble that accepts a single string argument:
+//  - The function should return a string that has each character repeated the number of times according to its position within the string argument.
+//  Additionally, each repeated section of characters should be separated by a hyphen (-).
+
+// Examples describe it best:
+
+//  mumble('X'); //=> 'X'
+//  mumble('abc'); //=> 'a-bb-ccc'
+//  mumble('121'); //=> '1-22-111'
+//  mumble('!A 2'); //=> '!-AA-   -2222'
+
+
+// ## Bonus
+// Capitalise the first character e.g. mumble('abc'); //=> 'A-Bb-Ccc'
+
+const mumble = function (string) {                                              // 'abc'
+  const chars = string.split('');                                               // ['a', 'b', 'c']
+  const nibbles = chars.map((char, i) => char.repeat(i + 1));                   // ['a', 'bb', 'ccc']
+  const capitalised = nibbles.map(x => x[0].toUpperCase() + x.slice(1));        // ['A', 'Bb', 'Ccc']
+  return capitalised.join('-');                                                 // 'Aa-Bb-Ccc'
+}
+
+const testStrings = {
+  'X': 'X',
+  'abc': 'A-Bb-Ccc',
+  '121': '1-22-111',
+  '!A 2': '!-AA-   -2222'
+};
+
+const TEST_mumble = function () {
+  for (let i = 0; i < Object.keys(testStrings).length; i++) {
+    const test = Object.keys(testStrings)[i];
+    console.log(
+      test,
+      mumble(test),
+      testStrings[test] === mumble(test));
+  }
+}
+
+TEST_mumble();
