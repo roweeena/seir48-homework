50819a63e5fb5379fc0c92c6b096b9a7f5e1bf4f
Goutham <goutham@Goutham-2.local>
2021-10-01 09:28:54 +1000

    computer scientist

---
+++ Goutham Masavarapu/week_02/week02_homework/day2_homework/js/trivia.js
@@ -0,0 +1,22 @@
+$( function() {
+    $( "#draggable" ).draggable();
+  } );
+
+
+  $( function() {
+      $( "#draggable1" ).draggable();
+    } );
+
+
+    $( function() {
+        $( "#draggable2" ).draggable();
+      } );
+
+      $( function() {
+          $( "#draggable3" ).draggable();
+        } );
+
+
+        $( function() {
+            $( "#draggable4" ).draggable();
+          } );
