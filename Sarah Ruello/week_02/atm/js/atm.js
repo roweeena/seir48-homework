e0bcb1cba0af674e84dd0108d27ff7b44ea32454
Sarah <sarahjune@gmail.com>
2021-10-03 01:34:27 +1000

    ATM tidied again

--- Sarah Ruello/week_02/atm/js/atm.js
+++ Sarah Ruello/week_02/atm/js/atm.js
@@ -1,93 +1,73 @@
-// * Keep track of the checking and savings balances somewhere
-let checkingBalance = 0;
-let savingsBalance = 0;
-let checkingAmount = Number($("#checking-amount").val());
-let savingsAmount = Number($("#savings-amount").val());
+$(document).ready(function () {
+  let checkingBalance = 0;
+  let savingsBalance = 0;
+  updater();
 
-// * Add functionality so that a user can deposit money into one of the bank accounts.
-// * Make sure you are updating the display and manipulating the HTML of the page
-// so a user can see the change.
+  // Checking deposit function:
+  $("#checking-deposit").click(function () {
+    let checkingAmount = Number($("#checking-amount").val());
+    checkingBalance += checkingAmount;
+    updater();
+  });
 
-$("#checking-deposit").click(function () {
-  let checkingAmount = Number($("#checking-amount").val());
-  checkingBalance += checkingAmount;
-  updater("checking");
-});
+  // Savings deposit function:
+  $("#savings-deposit").on("click", function () {
+    let savingsAmount = Number($("#savings-amount").val());
+    savingsBalance += savingsAmount;
+    updater();
+  });
 
-$("#savings-deposit").on("click", function () {
-  let savingsAmount = Number($("#savings-amount").val());
-  savingsBalance += savingsAmount;
-  updater("savings");
-});
+  // Checking withdrawal function:
+  $("#checking-withdraw").click(function () {
+    let checkingAmount = Number($("#checking-amount").val());
 
-// * Add functionality so that a user can withdraw money from one of the bank accounts.
-// * Make sure you are updating the display and manipulating the HTML of the page
-// so a user can see the change.
-// * Make sure the balance in an account can't go negative. If a user tries to
-// withdraw more money than exists in the account, ignore the transaction.
-// * What happens when the user wants to withdraw more money from the checking
-// account than is in the account? These accounts have overdraft protection, so if
-// a withdrawal can be covered by the balances in both accounts, take the checking
-// balance down to $0 and take the rest of the withdrawal from the savings account.
-// If the withdrawal amount is more than the combined account balance, ignore it.
-// * Make sure there is overdraft protection going both ways.
+    if (checkingAmount > checkingBalance + savingsBalance) {
+      updater();
+      console.log("not enough funds");
+    } else if (
+      checkingAmount >= checkingBalance &&
+      checkingAmount <= savingsBalance + checkingBalance
+    ) {
+      let emptyOutChecking = checkingAmount - checkingBalance;
+      savingsBalance -= emptyOutChecking;
+      checkingBalance = 0;
+      updater();
+    } else {
+      checkingBalance -= checkingAmount;
+      updater();
+    }
+  });
 
-$("#checking-withdraw").click(function () {
-  let checkingAmount = Number($("#checking-amount").val());
+  // Savings withdrawal function:
+  $("#savings-withdraw").on("click", function () {
+    let savingsAmount = Number($("#savings-amount").val());
 
-  if (checkingAmount > checkingBalance + savingsBalance) {
-    updater("checking");
-    console.log("not enough funds");
-  } else if (
-    checkingAmount >= checkingBalance &&
-    checkingAmount <= savingsBalance + checkingBalance
-  ) {
-    let emptyOutChecking = checkingAmount - checkingBalance;
-    savingsBalance -= emptyOutChecking;
-    checkingBalance = 0;
-    updater("checking");
-    updater("savings");
-  } else {
-    checkingBalance -= checkingAmount;
-    updater("checking");
-  }
-});
+    if (savingsAmount > savingsBalance + checkingBalance) {
+      updater();
+      console.log("not enough funds");
+    } else if (
+      savingsAmount >= savingsBalance &&
+      savingsAmount <= checkingBalance + savingsBalance
+    ) {
+      let emptyOutSavings = savingsAmount - savingsBalance;
+      checkingBalance -= emptyOutSavings;
+      savingsBalance = 0;
+      updater();
+    } else {
+      savingsBalance -= savingsAmount;
+      updater();
+    }
+  });
 
-$("#savings-withdraw").on("click", function () {
-  let savingsAmount = Number($("#savings-amount").val());
-
-  if (savingsAmount > savingsBalance + checkingBalance) {
-    updater("savings");
-    console.log("not enough funds");
-  } else if (
-    savingsAmount >= savingsBalance &&
-    savingsAmount <= checkingBalance + savingsBalance
-  ) {
-    let emptyOutSavings = savingsAmount - savingsBalance;
-    checkingBalance -= emptyOutSavings;
-    savingsBalance = 0;
-    updater("checking");
-    updater("savings");
-  } else {
-    savingsBalance -= savingsAmount;
-    updater("savings");
-  }
-});
-
-// * When the balance of the bank account is $0 the background of that bank account
-// should be red. It should be gray when there is money in the account.
-
-function updater(account) {
-  if (account === "savings") {
+  // Changes the balance display & background CSS:
+  function updater() {
     $("#savings-balance").html("$" + savingsBalance);
     if (savingsBalance !== 0) {
       $("#savings-balance").removeClass("zero");
     } else {
       $("#savings-balance").addClass("zero");
     }
-  }
 
-  if (account === "checking") {
     $("#checking-balance").html("$" + checkingBalance);
     if (checkingBalance !== 0) {
       $("#checking-balance").removeClass("zero");
@@ -95,5 +75,4 @@ function updater(account) {
       $("#checking-balance").addClass("zero");
     }
   }
-  console.log(account);
-}
+});
