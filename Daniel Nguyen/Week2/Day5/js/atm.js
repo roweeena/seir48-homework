8ea4b951b032f646d4022d22d03d143772fe7da8
daniel-nguyen <daniel.n.q.nguyen@gmail.com>
2021-10-04 06:11:29 +1100

    Add keyup event handlers for inputs

--- Daniel Nguyen/Week2/Day5/js/atm.js
+++ Daniel Nguyen/Week2/Day5/js/atm.js
@@ -4,35 +4,32 @@ const BANK = {
   savings: 0,
 
   deposit: function (accountName) {
-    const amount = DOM.getInputValue(accountName);
+    const amount = DOM[accountName + 'Input'];
     this[accountName] += amount;
-    DOM.updateBalanceDisplay(accountName);
   },
 
   withdraw: function (accountName) {
-    const amount = DOM.getInputValue(accountName);
+    const amount = DOM[accountName + 'Input'];
 
     if (amount <= this[accountName]) {
       this[accountName] -= amount;
-      DOM.updateBalanceDisplay(accountName);
     }
     // Overdraft; withdraws from the other account if possible
     else if (amount <= this.cheque + this.savings) {
       const otherAccount = accountName === 'cheque' ? 'savings' : 'cheque';
       this[otherAccount] -= amount - this[accountName];
       this[accountName] = 0;
-      DOM.updateBalanceDisplay(accountName);
-      DOM.updateBalanceDisplay(otherAccount);
     }
   }
 };
 
 // Contains all frontend DOM-related functions
 const DOM = {
-  getInputValue: function (accountName) {
-    return Number( $(`#${accountName}-amount`).val() );
-  },
+  // The current input values
+  chequeInput: null,
+  savingsInput: null,
 
+  // Re-renders account balance UIs
   updateBalanceDisplay: function (accountName) {
     const $div = $(`#${accountName}-balance`);
     $div.text(`$${BANK[accountName]}`);
@@ -42,23 +39,76 @@ const DOM = {
       $div.removeClass('zero');
     }
   },
+
+  enableWithdrawButton: function (acccountName) {
+    $(`#${acccountName}-withdraw`).prop('disabled', false);
+  },
+
+  disableWithdrawButton: function (acccountName) {
+    $(`#${acccountName}-withdraw`).prop('disabled', true);
+  }
+};
+
+// Event handlers
+const HANDLER = {
+  depositButton: function (accountName) {
+    BANK.deposit(accountName);
+    DOM.updateBalanceDisplay(accountName);
+    this.toggleWithdrawButtons();
+  },
+
+  withdrawButton: function (accountName) {
+    BANK.withdraw(accountName);
+    DOM.updateBalanceDisplay(accountName);
+    this.toggleWithdrawButtons();
+  },
+
+  // TODO: Prevent non-numbers from being typed
+  keyUp: function (accountName) {
+    const inputAmount = Number( $(`#${accountName}-amount`).val() );
+    DOM[accountName + 'Input'] = inputAmount;
+    this.toggleWithdrawButtons();
+  },
+
+  // Enables/disables withdraw buttons whenever input or balance changes
+  toggleWithdrawButtons: function () {
+    if (DOM.chequeInput <= BANK.cheque + BANK.savings && DOM.chequeInput > 0) {
+      DOM.enableWithdrawButton('cheque');
+    } else {
+      DOM.disableWithdrawButton('cheque');
+    }
+
+    if (DOM.savingsInput <= BANK.cheque + BANK.savings && DOM.savingsInput > 0) {
+      DOM.enableWithdrawButton('savings');
+    } else {
+      DOM.disableWithdrawButton('savings');
+    }
+  }
 };
 
 // Event listeners
 $(document).ready(function () {
-  $('#cheque-deposit').on('click', function() {
-    BANK.deposit('cheque');
+  $('#cheque-amount').on('keyup', function () {
+    HANDLER.keyUp('cheque');
   });
 
-  $('#cheque-withdraw').on('click', function() {
-    BANK.withdraw('cheque');
+  $('#cheque-deposit').on('click', function () {
+    HANDLER.depositButton('cheque');
   });
 
-  $('#savings-deposit').on('click', function() {
-    BANK.deposit('savings');
+  $('#cheque-withdraw').on('click', function () {
+    HANDLER.withdrawButton('cheque');
   });
 
-  $('#savings-withdraw').on('click', function() {
-    BANK.withdraw('savings');
+  $('#savings-amount').on('keyup', function (event) {
+    HANDLER.keyUp('savings');
+  });
+
+  $('#savings-deposit').on('click', function () {
+    HANDLER.depositButton('savings');
+  });
+
+  $('#savings-withdraw').on('click', function () {
+    HANDLER.withdrawButton('savings');
   });
 });
