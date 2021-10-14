47629e7cb05ed9a5ca4548344281d99fa9feba31
Joshua <joshua.mu02@hotmail.com>
2021-10-15 09:56:48 +1100

    Week 4? Work
    SQLite3
    Ruby
    PAIN

---
+++ Joshua Mu/Homework/13-10-21/main.rb
@@ -0,0 +1,24 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'httparty'
+
+get '/' do
+    erb :home
+end
+
+get '/book-info' do
+    book = params[:desired_book]
+    libraryURL = 'https://www.googleapis.com/books/v1/volumes?q=title:Ulysses'
+    @info = httparty.get libraryURL
+    i = 0
+
+    until i > libraryArray.length() do
+        if libraryArray[i][title] == book
+            return @result = @info["items"][i]["volumeInfo"]["imageLinks"]["thumbnail"]
+        end
+        i++
+    end
+    
+
+    erb :bookInfo
+end
