36b902c52a0edfdb49dcc03036e814c4c8aa903e
daniel-nguyen <daniel.n.q.nguyen@gmail.com>
2021-10-19 22:04:17 +1100

    CREATE routes/pages

--- Daniel Nguyen/Week5/Day2/afl/app/controllers/players_controller.rb
+++ Daniel Nguyen/Week5/Day2/afl/app/controllers/players_controller.rb
@@ -6,4 +6,18 @@ class PlayersController < ApplicationController
   def show
     @player = Player.find params[:id]
   end
+
+  def new
+  end
+
+  def add
+    player = Player.new
+    player.name = params[:name].titleize
+    player.position = params[:position].upcase
+    player.club = params[:club].upcase
+    player.scores = params[:scores]
+    player.save
+
+    redirect_to player_path(player.id)
+  end
 end
--- Daniel Nguyen/Week5/Day2/afl/config/routes.rb
+++ Daniel Nguyen/Week5/Day2/afl/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  root :to => 'players#index', :as => :index
-  # NEW
-  # CREATE
+  get '/players' => 'players#index', :as => :players
+  get '/players/new' => 'players#new', :as => :new_player
+  post '/players' => 'players#add'
   get '/players/:id' => 'players#show', :as => :player
   # EDIT
   # UPDATE
