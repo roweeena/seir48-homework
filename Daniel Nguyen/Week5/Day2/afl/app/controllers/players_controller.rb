db67e282206e9002d9e818164a040217c87274a4
daniel-nguyen <daniel.n.q.nguyen@gmail.com>
2021-10-19 20:01:58 +1100

    Initialise database

---
+++ Daniel Nguyen/Week5/Day2/afl/app/controllers/players_controller.rb
@@ -0,0 +1,11 @@
+class PlayersController < ApplicationController
+  def index
+    
+  end
+  # NEW
+  # CREATE
+  # SHOW
+  # EDIT
+  # UPDATE
+  # DELETE
+end
---
+++ Daniel Nguyen/Week5/Day2/afl/config/routes.rb
@@ -0,0 +1,3 @@
+Rails.application.routes.draw do
+  root :to => 'players#index'
+end
