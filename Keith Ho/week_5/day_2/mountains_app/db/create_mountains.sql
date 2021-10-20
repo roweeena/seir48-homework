ef164ce4cd0eedc6510f0a3ce72d6a35d75891df
Keith_hoka <keith_d_hau@hotmail.com>
2021-10-20 18:26:18 +1100

    homework_week5_day2

---
+++ Keith Ho/week_5/day_2/mountains_app/db/create_mountains.sql
@@ -0,0 +1,11 @@
+CREATE TABLE mountains (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT,
+  image TEXT,
+  elevation FLOAT,
+  latitude FLOAT,
+  longitude FLOAT,
+  country TEXT,
+  continent TEXT,
+  first_ascent INTEGER
+);
