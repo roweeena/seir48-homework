8aad40aa838051c150a01406cf9acdc409e03d7f
Just-Kundai <kundaikhuleya@outlook.com>
2021-11-02 10:37:25 +1100

    homework

--- Kundai Khuleya/week_7/googleBooks/js/books.js
+++ Kundai Khuleya/week_7/googleBooks/js/books.js
@@ -1,36 +1,21 @@
-const fetchBook = function (){
+const fetchCover = function (){
+  event.preventDefault();
+  const title = document.getElementById('title').value;
+  console.log('fetching!', title);
+
+ //get the title
   const xhr = new XMLHttpRequest();
-const bookInfo = document.getElementById('search').value
-  // callback!
-xhr.onreadystatechange = function () {
-  if (xhr.readyState !== 4) return;
+  xhr.open('GET', `https://www.googleapis.com/books/v1/volumes?q=title:${ title }`);
+  xhr.send();
 
-  const data = JSON.parse(xhr.responseText);
-  // console.log(data.items[0]);
-
-  const p = document.createElement('img');
-
-  const title = document.createElement('h1');
-
-  const author = document.createElement('h2');
-
-  const description = document.createElement('h3');
-
-  // const book = document.createElement []
-p.src = data.items[0].volumeInfo.imageLinks.thumbnail;
-document.body.appendChild(p);
-
-title.innerText = data.items[0].volumeInfo.title;
-document.body.appendChild(title);
-
-author.innerText = data.items[0].volumeInfo.authors;
-document.body.appendChild(author);
-
-description.innerText = data.items[0].volumeInfo.description;
-document.body.appendChild(description);
-};
-xhr.open('GET', `https://www.googleapis.com/books/v1/volumes?q=title:${ bookInfo }`);
-xhr.send();
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState !== 4) return;
+  //  console.log('we can now guarantee that the readyState is 4', xhr.readyState);
+    const data =JSON.parse(xhr.responseText);
+    const cover = data.items[0].volumeInfo.imageLinks.thumbnail
+    document.getElementById('cover').setAttribute('src', cover);
+    console.log(data); //not a string
+  };
 };
 
-document.getElementById('fetch').addEventListener('click', fetchBook);
+document.querySelector('form').addEventListener('submit', fetchCover);
