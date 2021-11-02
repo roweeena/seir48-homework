e0919c2f864c365ae5632382f665b2034fa3beee
daniel-nguyen <daniel.n.q.nguyen@gmail.com>
2021-11-02 11:20:41 +1100

    Refactor with jQuery

--- Daniel Nguyen/Week7/Day1/books/js/script.js
+++ Daniel Nguyen/Week7/Day1/books/js/script.js
@@ -1,24 +1,17 @@
 const fetchGoogleBooksAPI = function (event) {
   event.preventDefault(); // stay on this page; don't try to send the form data back to the server.
 
-  const xhr = new XMLHttpRequest();
-
-  const query = document.getElementById('query').value || 'Ulysses';
+  const query = $('#query')[0].value;
   const url = `https://www.googleapis.com/books/v1/volumes?q=title:${ query }`
-  xhr.open('GET', url);
-  xhr.send();
 
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState !== 4) return;
-
-    const data = JSON.parse(xhr.responseText).items[0].volumeInfo;
-
-    document.getElementById('title').innerHTML = data.title;
-    document.getElementById('authors').innerHTML = data.authors.join(', ');
-    document.getElementById('description').innerHTML = data.description;
-    document.getElementById('cover').src = data.imageLinks.thumbnail;
-    document.getElementById('cover').alt = `'${ data.title }' book cover'`;
-  };
+  $.ajax(url).done(function (result) {
+    const data = result.items[0].volumeInfo;
+    $('#title').text(data.title);
+    $('#authors').text(data.authors.join(', '));
+    $('#description').text(data.description);
+    $('#cover').attr('src', data.imageLinks.thumbnail);
+    $('#cover').attr('alt', `'${ data.title }' book cover'`);
+  });
 }
 
-document.querySelector('form').addEventListener('submit', fetchGoogleBooksAPI);
+$('form').on('submit', fetchGoogleBooksAPI);
