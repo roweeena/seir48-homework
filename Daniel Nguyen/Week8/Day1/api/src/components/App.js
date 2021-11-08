3d61e5d4a7df1bf82d72be8a9258beb099f67e50
daniel-nguyen <daniel.n.q.nguyen@gmail.com>
2021-11-08 16:26:27 +1100

    Initial commit

---
+++ Daniel Nguyen/Week8/Day1/api/src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>APIs!!</h1>
+        <p>Jeopardy!</p>
+      </div>
+    );
+  }
+}
+
+export default App;
