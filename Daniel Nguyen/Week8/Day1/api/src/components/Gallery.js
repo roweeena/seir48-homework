85d11387a5575b9525f92aa51e5fed7ae3e96148
daniel-nguyen <daniel.n.q.nguyen@gmail.com>
2021-11-09 10:13:21 +1100

    Gussy-up composer buttons

--- Daniel Nguyen/Week8/Day1/api/src/components/Gallery.js
+++ Daniel Nguyen/Week8/Day1/api/src/components/Gallery.js
@@ -8,13 +8,16 @@ class Gallery extends Component {
     super();
     this.state = {
       selected: null
-    }
+    };
   }
-  renderComposer(composer) {
+
+  renderComposerButton(composer) {
     return (
       <div className="composer-icon" key={ composer.id }>
-        <p>{ composer.complete_name }</p>
-        <img src={ composer.portrait } alt={ composer.complete_name } />
+        <button>
+          <p>{ composer.complete_name }</p>
+          <img src={ composer.portrait } alt={ composer.complete_name } />
+        </button>
       </div>
     );
   }
@@ -22,13 +25,40 @@ class Gallery extends Component {
   renderGallery() {
     return (
       <div className="gallery">
-        { this.props.composers.length > 0 ? this.props.composers.map(this.renderComposer) : '(No composers to show)' }
+        { this.props.composers.length > 0 ?
+          this.props.composers.map(this.renderComposerButton) :
+          '(No composers to show)' }
       </div>
     );
   }
 
+  showComposerInfo() {
+    const composer = this.state.selected;
+    if (composer) {
+      return (
+        <div>
+          <h3>{ composer.complete_name }</h3>
+          <p>Born: { composer.birth.slice(0, 4) }</p>
+          <p>Died: { composer.death.slice(0, 4) }</p>
+          <p>Period: { composer.epoch }</p>
+        </div>
+      )
+    } else {
+      return <div></div>
+    }
+  }
+
   render() {
-    return this.props.loading ? <Loading /> : this.renderGallery();
+    if (this.props.loading) {
+      return <Loading />
+    } else {
+      return (
+        <div>
+        { this.renderGallery() }
+        { this.showComposerInfo() }
+        </div>
+      );
+    }
   }
 };
 
