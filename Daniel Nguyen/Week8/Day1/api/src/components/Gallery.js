ec64733c39f297bb1e44b5996a133d2172dc24a5
daniel-nguyen <daniel.n.q.nguyen@gmail.com>
2021-11-09 09:50:57 +1100

    Convert Gallery from function to class, add state

--- Daniel Nguyen/Week8/Day1/api/src/components/Gallery.js
+++ Daniel Nguyen/Week8/Day1/api/src/components/Gallery.js
@@ -1,25 +1,35 @@
+import { Component } from 'react';
+
 import './css/Gallery.css';
 import Loading from './Loading';
 
-const Gallery = (props) => {
-  const renderComposer = function (composer) {
+class Gallery extends Component {
+  constructor() {
+    super();
+    this.state = {
+      selected: null
+    }
+  }
+  renderComposer(composer) {
     return (
       <div className="composer-icon" key={ composer.id }>
         <p>{ composer.complete_name }</p>
         <img src={ composer.portrait } alt={ composer.complete_name } />
       </div>
-    )
-  };
+    );
+  }
 
-  const renderGallery = () => {
+  renderGallery() {
     return (
       <div className="gallery">
-        { props.composers.length > 0 ? props.composers.map(renderComposer) : '(No composers to show)' }
+        { this.props.composers.length > 0 ? this.props.composers.map(this.renderComposer) : '(No composers to show)' }
       </div>
     );
-  };
+  }
 
-  return props.loading ? <Loading /> : renderGallery();
+  render() {
+    return this.props.loading ? <Loading /> : this.renderGallery();
+  }
 };
 
 export default Gallery;
